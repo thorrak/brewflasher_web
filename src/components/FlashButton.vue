0ffb14fc24d70c73388829926e5af44658f2bd9b
John <jbeeler@optictheory.com>
2022-01-19 18:48:47 -0500

    Fix clickability of button

--- src/components/FlashButton.vue
+++ src/components/FlashButton.vue
@@ -1,11 +1,9 @@
 <template>
   <esp-web-install-button :manifest="manifestUrl">
     <!-- eslint-disable-next-line vue/no-deprecated-slot-attribute-->
-    <button slot="activate">
-      <button type="button" class="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+    <button slot="activate" type="button" class="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
       <DownloadIcon class="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
       Flash to Controller
-      </button>
     </button>
   </esp-web-install-button>
 </template>
